144ba9370747211d720ac5c6a6e6dfaeae817d35
diego-aquino <diego.cruz001@gmail.com>
2020-08-07 17:01:44 -0300

    feat(utils): store options for 'select's

---
+++ src/utils/selectOptions.js
@@ -0,0 +1,22 @@
+export const subjects = [
+    'Artes',
+    'Biologia',
+    'Ciências',
+    'Educação física',
+    'Física',
+    'Geografia',
+    'História',
+    'Matemática',
+    'Português',
+    'Química'
+];
+
+export const weekdays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+];
