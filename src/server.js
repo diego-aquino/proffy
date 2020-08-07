e7b8a35925df1e1cf28c2cfd46bd369a785bdaea
diego-aquino <diego.cruz001@gmail.com>
2020-08-07 16:44:12 -0300

    feat(server): integrate proffy registration route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import nunjucks from 'nunjucks';
 
-import { pageLanding, pageStudy, pageGiveClasses } from './pages';
+import {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    proceedRegistration
+} from './pages';
 
 const server = express();
+
+// enable data receipt from req.body
+server.use(express.urlencoded({ extended: true }));
+
 server.use('/public', express.static('public'));
 
 nunjucks.configure('src/views', {
@@ -14,5 +23,6 @@ nunjucks.configure('src/views', {
 server.get('/', pageLanding);
 server.get('/study', pageStudy);
 server.get('/give-classes', pageGiveClasses);
+server.post('/proceed-registration', proceedRegistration);
 
 server.listen(5500);
